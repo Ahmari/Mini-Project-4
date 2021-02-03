ee242b283fdc9ae29ac283815cf0fa7401800c8e
Ahmari Miller <ahmari.miller@patelhs.org>
2021-02-03 17:11:06 +0000

    Categorized the exam scores using if/else statements within a new method.

--- Main.java
+++ Main.java
@@ -14,25 +14,22 @@ class Main {
     int score = getScore(topScore, numWrong);
     System.out.println("Your exam score is " + score + "%");
     boolean exam = studentScore(score);
-    int studentLevel = studentLevel(score);
-    System.out.println("Congrats! You are a level 5!");
+    System.out.println(studentLvl(score));
 
   }
-  // Create 2 methods of your choice. Your methods MUST have at least 1 parameter
-
-  // and must return a value.
+  // Create 2 methods of your choice. Your methods MUST have at least 1 parameter and must return a value.
   // Your methods have to return different types, for example: if your 1st method
-  // it is returning a boolean then maybe you'll want your 2nd method to return an
-  // int. It is up to you what you have them returning as long as they are
-  // returning 2 different types.
-  // Your methods need to be original! Please do not copy/paste the methods
-  // I've created with you in different assignments and use them here.
+  // it is returning a boolean then maybe you'll want your 2nd method to return an int.
+  // It is up to you what you have them returning as long as they are returning 2 different types
+  // Your methods need to be original! Please do not copy/paste the methods I've created with you in different assignments and use them here.
 
+  //methods
+  //This method gives the student their exam score when they enter the number they got wrong; Returns an int
   static int getScore(int topScore, int numWrong) {
     int score = topScore - numWrong;
     return score;
   }
-
+ //This method lets the student know if they passed the exam or not; Returns a boolean
   static boolean studentScore(int studentScore){
     boolean exam = true;
     if(studentScore >= 60){
@@ -44,11 +41,22 @@ class Main {
    return false;
    }
   }
-  static int getLevel(int studentLevel){
-   if(studentLevel >= 90);{
-    return studentLevel;
-   }
+//Gives the student level based on their score
+  static String studentLvl(int studentLvl){
+    if(studentLvl >= 90){
+      return "Congrats...you are a level 5!";
+    }else if(studentLvl >= 80){
+      return "Congrats...you are a level 4!";
+    }else if(studentLvl >= 70){
+      return "Congrats...You are a level 3!";
+    }else if(studentLvl >= 60){
+    return "You are a level 2...Maybe next time!";
+    }else if(studentLvl >= 50){
+    return "You are a level 1...Maybe next time!";
+    }else{
+    return "You are a level 0...Maybe next time!";
     }
   }
+}
 // 2 points extra credit for creativity!!!!
 // Do not forget to add me as a collaborator on your Github Repository!
